e81a4606820e0fb326b842e2962ebd05e0bb00c5
Hassani157 <hassani.assia2002@gmail.com>
2023-06-20 13:39:51 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,48 +1,45 @@
 #include <stdio.h>
 
 /**
- * main - Prints the Fibonacci sequence with dynamic handling of large numbers
+ * main - Prints the Fibonacci sequence
  *
- * Purpose: Fibonacci sequence without hardcoding, handling large numbers
+ * Purpose: Fibonacci sequence without hardcoding and handling large numbers
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	unsigned long i;
-	unsigned long bef = 1;
-	unsigned long aft = 2;
-	unsigned long l = 1000000000;
-	unsigned long bef1;
-	unsigned long bef2;
-	unsigned long aft1;
-	unsigned long aft2;
+    unsigned long i;
+    unsigned long bef = 1;
+    unsigned long aft = 2;
+    unsigned long bef1, bef2, aft1, aft2;
+    unsigned long l = 1000000000;
 
-	printf("%lu", bef);
+    printf("%lu", bef);
 
-	for (i = 1; i < 91; i++)
-	{
-		printf(", %lu", aft);
-		aft += bef;
-		bef = aft - bef;
-	}
+    for (i = 1; i < 91; i++)
+    {
+        printf(", %lu", aft);
+        aft += bef;
+        bef = aft - bef;
+    }
 
-	bef1 = bef / l;
-	bef2 = bef % l;
-	aft1 = aft / l;
-	aft2 = aft % l;
+    bef1 = bef / l;
+    bef2 = bef % l;
+    aft1 = aft / l;
+    aft2 = aft % l;
 
-	for (i = 92; i < 99; ++i)
-	{
-		printf(", %lu", aft1 + (aft2 / l));
-		printf("%lu", aft2 % l);
-		aft1 = aft1 + bef1;
-		bef1 = aft1 - bef1;
-		aft2 = aft2 + bef2;
-		bef2 = aft2 - bef2;
-	}
+    for (i = 92; i < 99; i++)
+    {
+        printf(", %lu", aft1 + (aft2 / l));
+        printf("%lu", aft2 % l);
+        aft1 = aft1 + bef1;
+        bef1 = aft1 - bef1;
+        aft2 = aft2 + bef2;
+        bef2 = aft2 - bef2;
+    }
 
-	printf("\n");
-	return 0;
+    printf("\n");
+    return 0;
 }
 
